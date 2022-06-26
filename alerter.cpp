bc8a01b9ddd243f461ab491254820098119d0bb4
ygn5cob <yuvaraja.gnanasekaran@in.bosch.com>
2022-06-26 13:58:10 +0200

    added test case foe alerter

--- alerter.cpp
+++ alerter.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <assert.h>
 
+#define THRESHOLD 250
+
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
@@ -8,7 +10,10 @@ int networkAlertStub(float celcius) {
     // Return 200 for ok
     // Return 500 for not-ok
     // stub always succeeds and returns 200
+    if(celcius < THRESHOLD)
     return 200;
+    else
+    return 500;
 }
 
 void alertInCelcius(float farenheit) {
@@ -25,7 +30,9 @@ void alertInCelcius(float farenheit) {
 
 int main() {
     alertInCelcius(400.5);
-    alertInCelcius(303.6);
+    assert(alertFailureCount == 0);
+    alertInCelcius(532.6);
+    assert(alertFailureCount == 1);
     std::cout << alertFailureCount << " alerts failed.\n";
     std::cout << "All is well (maybe!)\n";
     return 0;
